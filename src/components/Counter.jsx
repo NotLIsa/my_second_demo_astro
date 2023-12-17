8c2bbf393987862758a8c73b1678ee3e7fc18a8f
zhaoshali <1196580688@qq.com>
2023-12-17 19:15:30 +0800

    feat: 🚀 初始化astro框架

---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+
+import {createSignal} from 'solid-js'
+export function Counter(props){
+  const [count,setCount] = createSignal(props.count)
+  return( 
+    <div>
+      counter {count()}
+      <button onClick={()=>setCount(count()+1)} >click</button>
+    </div>
+  )
+}
